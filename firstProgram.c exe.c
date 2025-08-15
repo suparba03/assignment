dc1ece2a6b4893dcd9da12cab2c0c0c49daac5b1
suparba03 <suparba03@gmail.com>
2025-08-15 12:19:50 +0530

    Add files via upload

---
+++ firstProgram.c exe.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    float num1,num2,sum;
+    printf("Enter a number ");
+    scanf("%f",&num1);
+    printf("Enter a number ");
+    scanf("%f",&num2);
+    sum=num1+num2;
+    printf("Sum=%.2f\n",sum);
+    return 0;
+}
