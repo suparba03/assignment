6f5804647d3d52bcebbdb76dc3325c180d2151cb
suparba03 <suparba03@gmail.com>
2025-08-17 12:53:47 +0530

    Update firstProgram.c exe.c

--- firstProgram.c exe.c
+++ firstProgram.c exe.c
@@ -1,13 +1,8 @@
 #include<stdio.h>
 int main()
 {
-    float num1,num2,
-    printf("Enter a number ");
-    scanf("%f",&num1);
-    printf("Enter a number ");
-    scanf("%f",&num2);
-    sum=num1+num2;
-    printf("Sum=%.2f\n",sum);
+    printf("AU");
     return 0;
 }
+
 
